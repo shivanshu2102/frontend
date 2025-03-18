e247dbb1c22d1a27839c93cbd2278309d0f16bc9
shivanshu2102 <shivkaansh85@gmail.com>
2025-03-18 19:25:48 +0530

    auth chang commit for frontend

---
+++ authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    token: localStorage.getItem("token"),
+  },
+  reducers: {
+    loginSuccess: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      localStorage.setItem("token", action.payload.token);
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      localStorage.removeItem("token");
+    },
+  },
+});
+
+export const { loginSuccess, logout } = authSlice.actions;
+export default authSlice.reducer;
